a221c8934814b2ece8eec77eddc9e7f65626b0bf
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-03-14 18:13:20 -0300

    feat: adiciona a entidade User com a tabela

---
+++ src/main/java/com/javacore/spring_api_app/entity/User.java
@@ -0,0 +1,67 @@
+package com.javacore.spring_api_app.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "public_id", updatable = false)
+    private UUID publicId;
+
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String password;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Builder.Default
+    private Boolean deleted = false;
+
+    private Instant deletedAt;
+
+    public User(String email, String firstName, String lastName, String password) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        if (publicId == null) publicId = UUID.randomUUID();
+        if (createdAt == null) createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpate() {}
+}
---
+++ src/main/resources/db/migration/V1__create_table_users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users(
+    id BIGSERIAL PRIMARY KEY,
+    publicId UUID UNIQUE NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    deleted_at TIMESTAMP
+);
+
+CREATE UNIQUE INDEX users_email_unique_active ON users(email) WHERE deleted = FALSE;
